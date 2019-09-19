c65870749ac60da87922346af1aefda7bab1671b
Carlos Wilhelm <wilidoescher@gmail.com>
2019-09-19 18:02:25 -0300

    Contacts - configurando bugs 1

--- src/pages/form.vue
+++ src/pages/form.vue
@@ -2,7 +2,7 @@
   <q-dialog ref="dialog" @hide="onDialogHide">
     <q-card class="q-dialog-plugin" style="height: 800px">
       <div class="q-pa-md" style="max-width: 400px">
-        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+        <q-form @submit="verify" @reset="onReset" class="q-gutter-md">
           <q-input
             filled
             v-model="name"
@@ -16,41 +16,17 @@
             </template>
           </q-input>
 
-          <q-input
-            filled
-            v-model="surname"
-            label="Sobrenome"
-            hint
-            lazy-rules
-          />
+          <q-input filled v-model="surname" label="Sobrenome" hint lazy-rules />
 
-          <q-input
-            filled
-            v-model="company"
-            label="Empresa"
-            hint
-            lazy-rules
-          >
+          <q-input filled v-model="company" label="Empresa" hint lazy-rules>
             <template v-slot:prepend>
               <q-icon name="house" />
             </template>
           </q-input>
 
-          <q-input
-            filled
-            v-model="charge"
-            label="Cargo"
-            hint
-            lazy-rules
-          />
+          <q-input filled v-model="charge" label="Cargo" hint lazy-rules />
 
-          <q-input
-            filled
-            v-model="email"
-            label="E-mail"
-            hint
-            lazy-rules
-          >
+          <q-input filled v-model="email" label="E-mail" hint lazy-rules>
             <template v-slot:prepend>
               <q-icon name="email" />
             </template>
@@ -70,13 +46,7 @@
             </template>
           </q-input>
 
-          <q-input
-            filled
-            v-model="observation"
-            label="Observações"
-            hint
-            lazy-rules
-          >
+          <q-input filled v-model="observation" label="Observações" hint lazy-rules>
             <template v-slot:prepend>
               <q-icon name="warning" />
             </template>
@@ -105,6 +75,7 @@ export default {
   name: "Formulario",
   data() {
     return {
+      arr: [],
       id: null,
       name: null,
       surname: null,
@@ -118,10 +89,10 @@ export default {
     }
   },
 
- watch: {
-   contato: {
+  watch: {
+    contato: {
       immediate: true,
-      handler (val, oldVal) {
+      handler(val, oldVal) {
         if (val) {
           this.id = val.id
           this.name = val.nome
@@ -131,15 +102,13 @@ export default {
           this.charge = val.cargo
           this.phone = val.fone
           this.observation = val.observacoes
-         
         }
       }
     }
   },
 
-
   created() {},
-  props: ['contato'],
+  props: ["contato"],
 
   methods: {
     onSubmit() {
@@ -151,9 +120,9 @@ export default {
           message: "Salvo!"
         })
 
-        if(this.id){
-           axios
-            .put("http://localhost:3000/contatos/"+ this.id, {
+        if (this.id) {
+          axios
+            .put("http://localhost:3000/contatos/" + this.id, {
               nome: this.name,
               sobrenome: this.surname,
               empresa: this.company,
@@ -169,24 +138,69 @@ export default {
               console.log(error)
             })
         } else {
-
-        axios
-          .post("http://localhost:3000/contatos", {
-            nome: this.name,
-            sobrenome: this.surname,
-            empresa: this.company,
-            cargo: this.charge,
-            email: this.email,
-            fone: this.phone,
-            observacoes: this.observation
-          })
-          .then(function(response) {
-            console.log(response)
-          })
-          .catch(function(error) {
-            console.log(error)
-          })
+          axios
+            .post("http://localhost:3000/contatos", {
+              nome: this.name,
+              sobrenome: this.surname,
+              empresa: this.company,
+              cargo: this.charge,
+              email: this.email,
+              fone: this.phone,
+              observacoes: this.observation
+            })
+            .then(function(response) {
+              console.log(response)
+            })
+            .catch(function(error) {
+              console.log(error)
+            })
         }
+      }
+    },
+    verify() {
+      var data = {
+        nome: ["Bob", "Smith"],
+        idade: 32,
+        sexo: "masculino",
+        interesses: ["música", "esquiar"],
+        bio: function() {
+          alert(
+            this.nome[0] +
+              " " +
+              this.nome[1] +
+              " tem " +
+              this.idade +
+              " anos de idade. Ele gosta de " +
+              this.interesses[0] +
+              " e " +
+              this.interesses[1] +
+              "."
+          )
+        },
+        saudacao: function() {
+          alert("Oi! Eu sou " + this.nome[0] + ".")
+        }
+      }
+
+      data.nome.push("Bob")
+
+      data.nome.push("CArlos")
+
+      console.log(data.nome)
+
+      for (let key in data.nome) {
+        //bob
+        let achado = data.nome.filter(function(element) {
+          return element == data.nome[key]
+        })
+
+        // console.log(data.nome[key] + ' - '+ achado.length)
+
+        if (achado.length > 1) {
+          console.log("Foi encontrado repetidos, deseja mesclar? " + achado)
+        }
+
+        // console.log(data.nome[key])
       }
     },
 
@@ -199,7 +213,7 @@ export default {
       this.phone = null
       this.observation = null
       this.accept = false
-      console.log('aheu')
+      console.log("aheu")
     },
     show() {
       this.$refs.dialog.show()
